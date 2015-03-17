329df76e371ccda092ba5122da2607390e0ec13b
uidev547 <akiran547@gmail.com>
2015-03-17 19:12:43 +0530

    Update index.js
    with configurable options.
    uri({ 
      output: 'template',
      templateUrl: 'less-variables.template'
    }

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ function getOptions(optset) {
 
 function URIfy(overrides) {
     var options = getOptions(overrides);
+	
+	
 
     // creating a stream through which each file will pass
     var stream = through.obj(function(file, enc, cb) {
@@ -30,12 +32,17 @@ function URIfy(overrides) {
             var filePath = file.path.replace(file.cwd + path.sep, "");
             var header = "data:" + mime.lookup(file.path) + ";" + options.encoding +",";
             var dURI = header + encodeURIComponent(file.contents.toString(options.encoding));
-
-            // This needs to be a JSON flag
+			var fileName = path.basename(filePath,path.extname( filePath ) );
+			fs = require('fs');
+			var template = fs.readFileSync(options.templateUrl, 'utf8');
+			// This needs to be a JSON flag
             if(options.output == "json") {
                 var o = {};
                 o[filePath] = dURI;
                 dURI = JSON.stringify(o);
+            }
+			if(options.output == "template") {
+                dURI = template.replace( /{{name}}/g,fileName ).replace( /{{datauri}}/g, dURI );
             }
 
             file.contents = new Buffer(dURI);
