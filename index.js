47a98da26662d178ec0f44dbf37648a43635c776
uidev547 <akiran547@gmail.com>
2015-03-17 19:13:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,16 +32,16 @@ function URIfy(overrides) {
             var filePath = file.path.replace(file.cwd + path.sep, "");
             var header = "data:" + mime.lookup(file.path) + ";" + options.encoding +",";
             var dURI = header + encodeURIComponent(file.contents.toString(options.encoding));
-			var fileName = path.basename(filePath,path.extname( filePath ) );
-			fs = require('fs');
-			var template = fs.readFileSync(options.templateUrl, 'utf8');
+            var fileName = path.basename(filePath,path.extname( filePath ) );
+            fs = require('fs');
+            var template = fs.readFileSync(options.templateUrl, 'utf8');
 			// This needs to be a JSON flag
             if(options.output == "json") {
                 var o = {};
                 o[filePath] = dURI;
                 dURI = JSON.stringify(o);
             }
-			if(options.output == "template") {
+            if(options.output == "template") {
                 dURI = template.replace( /{{name}}/g,fileName ).replace( /{{datauri}}/g, dURI );
             }
 
